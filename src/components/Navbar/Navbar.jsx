087094541938cb7b9cf3537d1b3ff1ae097a717e
Gajanan Jadhav <111537135+gj701763@users.noreply.github.com>
2025-05-07 14:55:13 +0530

    Update Navbar.jsx
    linkedin link are connect

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -79,7 +79,7 @@ const Navbar = () => {
             <FaGithub size={24} />
           </a>
           <a
-            href="#"
+            href="https://www.linkedin.com/in/gajanan-jadhav-999266228/"
             target="_blank"
             rel="noopener noreferrer"
             className="text-gray-300 hover:text-[#8245ec]"
