cd9e2f0d515a68cc7b0788bbf15f106c512b99c4
gj701763 <gj701763@gmail.com>
2025-07-14 10:10:25 +0530

    Skill page add leetcode logos

--- src/constants.js
+++ src/constants.js
@@ -22,6 +22,7 @@ import cppLogo from './assets/tech_logo/cpp.png';
 import javaLogo from './assets/tech_logo/java.png';
 import pythonLogo from './assets/tech_logo/python.png';
 import typescriptLogo from './assets/tech_logo/typescript.png';
+import leetcodeLogo from './assets/tech_logo/leetcode.png'; // ✅ Fixed path
 import gitLogo from './assets/tech_logo/git.png';
 import githubLogo from './assets/tech_logo/github.png';
 import vscodeLogo from './assets/tech_logo/vscode.png';
@@ -51,42 +52,63 @@ import npmLogo from './assets/work_logo/spotify.png'
 
 
 
-export const SkillsInfo = [
-  {
+export const SkillsInfo = [{
     title: 'Frontend',
-    skills: [
-      { name: 'HTML', logo: htmlLogo },
-      { name: 'CSS', logo: cssLogo },
-      { name: 'SASS', logo: sassLogo },
-      { name: 'JavaScript', logo: javascriptLogo },
-      { name: 'React JS', logo: reactjsLogo },
-      { name: 'Tailwind CSS', logo: tailwindcssLogo },
-      { name: 'Bootstrap', logo: bootstrapLogo },
+    skills: [{
+        name: 'HTML',
+        logo: htmlLogo
+      },
+      {
+        name: 'CSS',
+        logo: cssLogo
+      },
+      {
+        name: 'SASS',
+        logo: sassLogo
+      },
+      {
+        name: 'JavaScript',
+        logo: javascriptLogo
+      },
+      {
+        name: 'React JS',
+        logo: reactjsLogo
+      },
+      {
+        name: 'Tailwind CSS',
+        logo: tailwindcssLogo
+      },
+      {
+        name: 'Bootstrap',
+        logo: bootstrapLogo
+      },
     ],
   },
   {
     title: 'Backend',
     skills: [
-      { name: 'Springboot', logo: springbootLogo },
-      { name: 'Node JS', logo: nodejsLogo },
-      { name: 'Express JS', logo: expressjsLogo },
-      { name: 'MySQL', logo: mysqlLogo },
-      { name: 'MongoDB', logo: mongodbLogo },
-      { name: 'Firebase', logo: firebaseLogo },
+      {name: 'Springboot',logo: springbootLogo},
+      {name: 'Node JS',logo: nodejsLogo },
+      {name: 'Express JS', logo: expressjsLogo
+      },
+      {name: 'MySQL', logo: mysqlLogo},
+      {name: 'MongoDB',logo: mongodbLogo},
+      { name: 'Firebase',logo: firebaseLogo },
     ],
   },
   {
     title: 'Languages',
     skills: [
-      { name: 'C', logo: cLogo },
-      { name: 'C++', logo: cppLogo },
-      { name: 'Java', logo: javaLogo },
-      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'C',logo: cLogo},
+      {name: 'C++', logo: cppLogo},
+      {name: 'Java',logo: javaLogo},
+      { name: 'JavaScript',logo: javascriptLogo},
     ],
   },
   {
     title: 'Tools',
     skills: [
+      { name: 'LeetCode', logo: leetcodeLogo },
       { name: 'Git', logo: gitLogo },
       { name: 'GitHub', logo: githubLogo },
       { name: 'VS Code', logo: vscodeLogo },
@@ -95,124 +117,119 @@ export const SkillsInfo = [
       { name: 'Vercel', logo: vercelLogo },
       { name: 'Netlify', logo: netlifyLogo },
       { name: 'Figma', logo: figmaLogo },
-    ],
+    ]
+
+
   },
 ];
 
 export const Certificationinfo = [
 
   {
-    title: 'Internship ', 
-    pdf : "https://drive.google.com/file/d/18Qfg30V-_b0WS790bncH_nQC3jHzeZk8/view?usp=drive_open",
+    title: 'Internship ',
+    pdf: "https://drive.google.com/file/d/18Qfg30V-_b0WS790bncH_nQC3jHzeZk8/view?usp=drive_open",
     name: 'Internship ',
     desc: "A Java Internship certification recognizes hands-on experience in Java development, typically involving real-world projects or industry-based training. It covers advanced concepts such as collections, multithreading, file handling, JDBC, and basic frameworks, demonstrating practical proficiency in building and maintaining Java applications.",
   },
   {
     title: 'Java',
     name: 'Java (Basic)',
-    pdf:"https://drive.google.com/file/d/1FNDdHF9ZYtxhoKCX_cfAG0YX6wYfK85f/view",
+    pdf: "https://drive.google.com/file/d/1FNDdHF9ZYtxhoKCX_cfAG0YX6wYfK85f/view",
     desc: "A Java (Basic) certification validates foundational knowledge of Java programming, including syntax, data types, loops, and object-oriented concepts. It demonstrates the ability to write and understand simple Java programs",
 
   },
   {
     title: 'JavaScript',
-    name : 'JavaScript',
-    pdf : "https://drive.google.com/file/d/1KskmBLxw15Z_CICZpzi7KG9A2X5IuiWl/view?usp=drive_open",
+    name: 'JavaScript',
+    pdf: "https://drive.google.com/file/d/1KskmBLxw15Z_CICZpzi7KG9A2X5IuiWl/view?usp=drive_open",
     desc: "A JavaScript (Basic) certification validates foundational knowledge of JavaScript programming, including variables, data types, functions, loops, and basic DOM manipulation. It demonstrates the ability to write and understand simple client-side scripts in a web development environment.",
   },
   {
     title: 'Web Development',
     pdf: "https://drive.google.com/file/d/1znfiQGfVDkoOSn7jhUwlysndflN956l_/view",
-    name : 'Web Development',
+    name: 'Web Development',
     desc: "A Web Development certification validates practical skills in building modern websites and web applications. It covers core technologies like HTML, CSS, JavaScript, and may include frameworks such as React, Node.js, and Express. It demonstrates the ability to create responsive, interactive, and full-stack web solutions",
 
   },
-  
+
 ];
-  export const experiences = [
-    {
-      id: 0,
-      
-    },
-    {
-      
-    },
-    {
-      id: 2,
-      
-    },
-  ];
-  
-  export const education = [
-    {
-      id: 0,
-      img: glaLogo,
-      school: "Govt.College of Engineering, Chandrapur",
-      date: "Dec 2021 - June 2025",
-      grade: "7.5 CGPA",
-      desc: "I have completed my Bachelor's degree (B.E.) in Computer Science and Engineering from Gondwana University. During my time at Gondwana, I built a strong foundation in programming, software development, and core computer science concepts. I studied subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering.My academic journey at Gondwana University has played a vital role in developing my technical capabilities and shaping my professional growth.",
-      degree: "Bachelor of Engineering - B.E (Computer Science & Engineering)",
-    },
-    {
-      id: 1,
-      img: vpsLogo,
-      school: "Vivekand Vidhya Mandir Hiwara Ashram,Mehkar",
-      date: "June 2019 - March 2021",
-      grade: "87.17%",
-      desc: "I completed my Class 12 education from Vivekanand Vidya Mandir, Hiwara Ashram, Mehkar, under the Maharashtra State Board. I pursued the PCMB group, studying Physics, Chemistry, Mathematics, and Biology, along with other core subjects.",
-      degree: "Maharashtra State Board(XII) - PCMB ",
-    },
-    {
-      id: 2,
-      img: maha,
-      school: "SMT.Indira Gandhi High School,Ramnagar",
-      date: "June 2018 - March 2019",
-      grade: "87.5%",
-      desc: "I completed my class 10 education from SMT.Indira Gandhi School, Ramnagar, under the Maharashtra State Board, where I studied Science with Math.",
-      degree: "Maharashtra State Board(X), Marathi Medium",
-    },
-  ];
-  
-  export const projects = [
-    {
-      id: 0,
-      title: "Local Connect",
-      description:
-        "LocalConnect is a marketplace platform connecting users with nearby local sellers. It provides a clean, dark-themed UI with a focus on product discovery, rating, and interaction. The sidebar and top navigation allow intuitive filtering and exploration",
-      image: githubdetLogo,
-      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
-      github: "https://github.com/gj701763/markets/",
-      webapp: "https://localconnects.vercel.app/",
-    },
-    {
-      id: 1,
-      title: "Netflix Clone",
-      description:
-        "The Netflix Clone is a full-stack web application that replicates the core functionality and user interface of Netflix, the popular video streaming platform. It allows users to browse, search, and watch trailers of movies and TV shows, offering a responsive, engaging UI similar to the real Netflix",
-      image: csprepLogo,
-      tags: ["React JS","HTML", "CSS", "JavaScript"],
-      github: "https://github.com/gj701763/NETFLIX_CLONE",
-      webapp: "https://landingpage-kringle-0f4e2d.netlify.app/",
-    },
-    {
-      id: 2,
-      title: "LandingPage WebSite",
-      description:
-        "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it landing page web app",
-      image: movierecLogo,
-      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
-      github: "https://github.com/gj701763/LandingPage",
-      webapp: "https://sage-duckanoo-3b6240.netlify.app/",
-    },
-    {
-      id: 3,
-      title: "Spotify clone",
-      description:
-        "The Spotify Clone is a full-stack web application that replicates the core functionality and user interface of Netflix, the popular video streaming platform. It allows users to browse, search, and watch trailers of movies and TV shows, offering a responsive, engaging UI similar to the real Spotify",
-      image: npmLogo,
-      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
-      github: "https://github.com/gj701763/Tailwind_Projects",
-      webapp: "https://spotify-sprightly-kleicha-83ea48.netlify.app/spotify",
-      },
-    
-  ];  
+export const experiences = [{
+    id: 0,
+
+  },
+  {
+
+  },
+  {
+    id: 2,
+
+  },
+];
+
+export const education = [{
+    id: 0,
+    img: glaLogo,
+    school: "Govt.College of Engineering, Chandrapur",
+    date: "Dec 2021 - June 2025",
+    grade: "7.5 CGPA",
+    desc: "I have completed my Bachelor's degree (B.E.) in Computer Science and Engineering from Gondwana University. During my time at Gondwana, I built a strong foundation in programming, software development, and core computer science concepts. I studied subjects such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering.My academic journey at Gondwana University has played a vital role in developing my technical capabilities and shaping my professional growth.",
+    degree: "Bachelor of Engineering - B.E (Computer Science & Engineering)",
+  },
+  {
+    id: 1,
+    img: vpsLogo,
+    school: "Vivekand Vidhya Mandir Hiwara Ashram,Mehkar",
+    date: "June 2019 - March 2021",
+    grade: "87.17%",
+    desc: "I completed my Class 12 education from Vivekanand Vidya Mandir, Hiwara Ashram, Mehkar, under the Maharashtra State Board. I pursued the PCMB group, studying Physics, Chemistry, Mathematics, and Biology, along with other core subjects.",
+    degree: "Maharashtra State Board(XII) - PCMB ",
+  },
+  {
+    id: 2,
+    img: maha,
+    school: "SMT.Indira Gandhi High School,Ramnagar",
+    date: "June 2018 - March 2019",
+    grade: "87.5%",
+    desc: "I completed my class 10 education from SMT.Indira Gandhi School, Ramnagar, under the Maharashtra State Board, where I studied Science with Math.",
+    degree: "Maharashtra State Board(X), Marathi Medium",
+  },
+];
+
+export const projects = [{
+    id: 0,
+    title: "Local Connect",
+    description: "LocalConnect is a marketplace platform connecting users with nearby local sellers. It provides a clean, dark-themed UI with a focus on product discovery, rating, and interaction. The sidebar and top navigation allow intuitive filtering and exploration",
+    image: githubdetLogo,
+    tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/gj701763/markets/",
+    webapp: "https://localconnects.vercel.app/",
+  },
+  {
+    id: 1,
+    title: "Netflix Clone",
+    description: "The Netflix Clone is a full-stack web application that replicates the core functionality and user interface of Netflix, the popular video streaming platform. It allows users to browse, search, and watch trailers of movies and TV shows, offering a responsive, engaging UI similar to the real Netflix",
+    image: csprepLogo,
+    tags: ["React JS", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/gj701763/NETFLIX_CLONE",
+    webapp: "https://landingpage-kringle-0f4e2d.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "LandingPage WebSite",
+    description: "A React-based web application that provides movie recommendations based on different criteria, such as genres, user preferences, and popular trends. The intuitive design and smooth experience make it landing page web app",
+    image: movierecLogo,
+    tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/gj701763/LandingPage",
+    webapp: "https://sage-duckanoo-3b6240.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Spotify clone",
+    description: "The Spotify Clone is a full-stack web application that replicates the core functionality and user interface of Netflix, the popular video streaming platform. It allows users to browse, search, and watch trailers of movies and TV shows, offering a responsive, engaging UI similar to the real Spotify",
+    image: npmLogo,
+    tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+    github: "https://github.com/gj701763/Tailwind_Projects",
+    webapp: "https://spotify-sprightly-kleicha-83ea48.netlify.app/spotify",
+  },
+
+];
